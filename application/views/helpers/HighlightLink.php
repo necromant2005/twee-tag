504b47b499235c80b1498c3c723789d542a68284
Rostyslav Mykhajliw <necromant2005@gmail.com>
2011-08-08 22:17:28 +0300

    link highlight

--- application/views/helpers/HighlightLink.php
+++ application/views/helpers/HighlightLink.php
@@ -2,13 +2,13 @@
 
 namespace Application\View\Helper;
 
-use Zend\View\Helper;
+use Zend\View\Helper\AbstractHelper;
 
 class HighlightLink extends AbstractHelper
 {
     public function direct($text)
     {
-
+        return preg_replace('~(http://[^ ]+)~si', '<a href="\\1">\\1</a>', $text);
     }
 }
 
